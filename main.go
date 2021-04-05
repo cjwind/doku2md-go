fb53217defc79704bf8a5522bb9ab8aa77413e5c
cjwind <cwentsai@gmail.com>
2021-04-05 19:45:48 +0800

    Implement main()

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
-package doku2md
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/cjwind/doku2md-go/doku2md"
+	"io/ioutil"
+)
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		fmt.Println("doku2md-go [filepath]")
+		return
+	}
+
+	filepath := flag.Arg(0)
+
+	content, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	converter := doku2md.Converter{}
+	output := converter.DokuToMd(string(content))
+	fmt.Print(output)
+}
